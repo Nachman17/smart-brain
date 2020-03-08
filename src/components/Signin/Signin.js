10dde35e6345376b08f2e910e9195d2443241a04
nachman <nachman17@gmail.com>
2020-03-09 01:20:22 +0200

    url fixes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://git.heroku.com/floating-caverns-56347.git:3000/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
